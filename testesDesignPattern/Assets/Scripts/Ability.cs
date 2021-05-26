54231f0c76aee7a77c473bca275ab970f76c37cb
Gabriel Lo Monaco Honório <75794887+Gabriel-LF@users.noreply.github.com>
2021-05-26 17:18:16 -0300

    testando

---
+++ testesDesignPattern/Assets/Scripts/Ability.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static UnityEngine.Debug;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SimpleFactory
+{
+    public abstract class Ability
+    {
+        public abstract void Process();
+    }
+
+    public class StartFireAbility : Ability
+    {
+        public override void Process()
+        {
+            // Executar habilidade
+            Debug.Log("SOLTANDO FOGO");
+        }
+    }
+    public class HealSelfAbility : Ability
+    {
+        public override void Process()
+        {
+            // Executar habilidade
+            Debug.Log("SE CURANDO");
+        }
+    }
+
+    public class AbilityFactory
+    {
+        public static Ability GetAbility(string abilityType)
+        {
+            switch(abilityType)
+            {
+                case "fire":
+                    return new StartFireAbility();
+                case "heal":
+                    return new HealSelfAbility();
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ testesDesignPattern/Assets/Scripts/ButtonPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleFactory;
+
+public class ButtonPanel : MonoBehaviour
+{
+    public void Fire()
+    {
+        AbilityFactory.GetAbility("fire");
+        Debug.Log("APERTEI FOGO");
+    }
+    public void Heal()
+    {
+        AbilityFactory.GetAbility("heal");
+    }
+}
