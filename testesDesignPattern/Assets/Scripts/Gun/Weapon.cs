127f09e965034c7ccb94549f854d20743aff8433
Gabriel Lo Monaco Honório <75794887+Gabriel-LF@users.noreply.github.com>
2021-06-01 17:06:48 -0300

    Gerador de armas

---
+++ testesDesignPattern/Assets/Scripts/Gun/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float damage, speed, knockback;
+    public int durability, maxDurability;
+    public string weaponName;
+
+    public void GetStats()
+    {
+
+    }
+}
---
+++ testesDesignPattern/Assets/Scripts/Gun/WeaponManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public static WeaponManager wm;
+
+    public GameObject blankWeapon;
+    public GameObject[] handles, barrels, muzzles;
+
+    public void GenerateWeapon(Vector2 pos)
+    {
+        GameObject weapon = Instantiate(blankWeapon, (Vector2)pos, Quaternion.identity);
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        wm = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
