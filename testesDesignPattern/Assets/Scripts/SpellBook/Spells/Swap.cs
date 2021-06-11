ca6f103c760794a46432e5bf8aaaf11437411ad8
Gabriel Lo Monaco Honório <75794887+Gabriel-LF@users.noreply.github.com>
2021-06-11 17:02:13 -0300

    Swap

---
+++ testesDesignPattern/Assets/Scripts/SpellBook/Spells/Swap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Swap : Spell
+{
+    public override string Name => "Swap";
+    float coolTime = 1;
+
+    public override void Process()
+    {
+        cooldown = GameObject.Find(Name + " Cooldown");
+        ui = GameObject.Find(Name + " Button");
+
+        Debug.Log("SUMONEI " + Name);
+        cooldown.GetComponent<Cooldown>().cooldownTime = coolTime;
+        cooldown.GetComponent<Cooldown>().timerActive = true;
+        ui.GetComponent<Slider>().maxValue = coolTime;
+    }
+}
